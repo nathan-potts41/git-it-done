34696f6603a0ccb7a4f017e2fa0e9592610227a1
nathan-potts41 <pottsandpans41@hotmail.com>
2020-11-06 11:01:28 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,11 +3,17 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function () {
+
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
 
+    }
+    else {
+        document.location.replace("./index.html");
+    }
 }
 
 var getRepoIssues = function (repo) {
@@ -26,7 +32,7 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
     console.log(repo);
